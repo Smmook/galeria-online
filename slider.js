cffcc9788ea47a46b5911d45bd71435dd367fef2
Smmook <diegobix97@gmail.com>
2022-04-30 14:37:13 +0200

    Nuevo Slider v2

--- slider.js
+++ slider.js
@@ -1,19 +1,24 @@
 const slide = document.querySelector('.slide');
 const slideImgs = document.querySelectorAll('.slide img');
+const sliderDots = document.querySelectorAll('.slider-dots button');
 let slideIndex = 1;
 let isMoving = false;
 
 slide.insertAdjacentElement('afterbegin', slideImgs[slideImgs.length - 1].cloneNode(true));
 slide.insertAdjacentElement('beforeend', slideImgs[0].cloneNode(true));
 
+const handleDots = () => {
+  document.querySelector('.slider-dots button.active').classList.remove('active');
+  sliderDots[slideIndex - 1].classList.add('active');
+}
+
 const moveSlides = () => {
   slide.style.transform = `translateX(-${slideIndex * 100}%)`;
-  const slidesArray = slide.querySelectorAll('img');
 }
 
 const moveHandler = (direction) => {
-  isMoving = true;
   slide.style.transition = 'transform .8s ease-in-out';
+  isMoving = true;
   if (direction === 'right') {
     slideIndex++;
   } else {
@@ -22,7 +27,18 @@ const moveHandler = (direction) => {
   moveSlides();
 }
 
+const resetSliderAuto = () => {
+  clearInterval(sliderAuto);
+  sliderAuto = setInterval(() => {
+    moveHandler('right');
+  }, 3500);
+}
+
 window.onload = () => moveSlides();
+
+let sliderAuto = setInterval(() => {
+  moveHandler('right');
+}, 3500);
 
 window.addEventListener('resize', () => {
   const someSliderImg = slide.querySelectorAll('img')[0];
@@ -31,11 +47,14 @@ window.addEventListener('resize', () => {
 
 document.querySelector('.slider-btn-left').addEventListener('click', () => {
   if (isMoving) return;
+  resetSliderAuto();
   moveHandler('left');
+  
 });
 
 document.querySelector('.slider-btn-right').addEventListener('click', () => {
   if (isMoving) return;
+  resetSliderAuto();
   moveHandler('right');
 });
 
@@ -51,4 +70,21 @@ slide.addEventListener('transitionend', () => {
     slideIndex = 1;
     moveSlides();
   }
+  handleDots();
+})
+
+sliderDots.forEach(dot => {
+  dot.addEventListener('click', e => {
+    if (isMoving) {
+      setTimeout(() => {
+        isMoving = false;
+      }, 800);
+      return;
+    }
+    slide.style.transition = 'transform .8s ease-in-out';
+    slideIndex = e.target.id;
+    isMoving = true;
+    handleDots();
+    moveSlides();
+  })
 })
